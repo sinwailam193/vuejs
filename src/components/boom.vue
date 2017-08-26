56e79fcf6880f71ac048289ea7f9843c4bd0a279
sinwailam193 <sinwailam193@hotmail.com>
2017-08-25 23:31:06 -0700

    added basics

---
+++ src/components/boom.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>This is {{ msg }}</p>
+</template>
+
+<script>
+    export default {
+        name: "boom",
+        props: {
+            msg: {
+                default: "Foobar",
+                type: String
+            }
+        }
+    };
+</script>
---
+++ src/components/users.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="users">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "users"
+    };
+</script>
+
+<style>
+</style>
