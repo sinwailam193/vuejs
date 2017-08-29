c3a3e6d091028a496f4d0fd3bc08a84cba535a34
sinwailam193 <sinwailam193@hotmail.com>
2017-08-29 10:41:49 -0700

    took out all controllers

--- src/components/boom.vue
+++ src/components/boom.vue
@@ -2,4 +2,16 @@
     <p>This is {{ msg }}</p>
 </template>
 
-<script src="@/controllers/boom.js"></script>
+<script>
+    import Vue from "vue";
+    import Component from "vue-class-component";
+
+    @Component({
+        props: {
+            msg: {
+                type: String
+            }
+        }
+    })
+    export default class Boom extends Vue {}
+</script>
--- src/components/users.vue
+++ src/components/users.vue
@@ -21,4 +21,47 @@
     </div>
 </template>
 
-<script type="text/javascript" src="@/controllers/users.js"></script>
+<script>
+    import Vue from "vue";
+    import Component from "vue-class-component";
+
+    @Component({
+        title: "Users"
+    })
+    export default class Users extends Vue {
+        newUser = {
+            name: "",
+            email: ""
+        };
+
+        get page() {
+            return this.$store.getters.USERS_VIEW;
+        }
+
+        created() {
+            this.$store.dispatch("GET_USERS");
+        }
+
+        onSubmit(event) {
+            event.preventDefault();
+            const { newUser: { name, email }, page: { users } } = this;
+            if (name && email) {
+                users.push({
+                    name,
+                    email,
+                    contacted: false
+                });
+                this.newUser = { name: "", email: "" };
+            }
+        }
+
+        onClick(index) {
+            const { users, isToggle } = this.page;
+            if (index === "Toggle") {
+                this.$store.commit("CHANGE_STATE", { isToggle: !isToggle });
+            } else {
+                users.splice(index, 1);
+            }
+        }
+    }
+</script>
